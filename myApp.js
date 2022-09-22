32a9ce9f53ae58eaa4dc735802bed3608df100c8
DDaniel-Dev <ddaniel.atx@gmail.com>
2022-09-21 22:54:20 -0500

    feat: COMPLETED - MongoDb & Mongoose Lessons

--- myApp.js
+++ myApp.js
@@ -122,11 +122,17 @@ const removeManyPeople = (done) => {
 };
 
 
-// -- 
+// -- #12. Chain Search Query Helpers to Narrow Search Results -- //
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch})
+    .sort({ name: 1})
+    .limit(2)
+    .select({age: 1, age: 0})
+    .exec((err, burritoPeople) => {
+      if (err) return console.error(err);
+      done(null, burritoPeople);
+    });
 };
 
 /** **Well Done !!**
